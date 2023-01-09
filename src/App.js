d6e4be0f8e0472b2147d0d695c91492c862f2517
Michael Stewart <michael.stewart@revcapture.ai>
2023-01-10 10:51:49 +1100

    Initial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          ACME Home Rental Insurance
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
